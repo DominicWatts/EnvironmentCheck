a68ec95e9df09c388eaf2cdce6f4b7a5d83507e9
Dominic Watts <dominic@xigen.co.uk>
2020-03-18 15:12:28 +0000

    base

---
+++ Console/Command/Php.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace Xigen\PhpCheck\Console\Command;
+
+use Magento\Setup\Controller\Environment;
+use Magento\Setup\Controller\ReadinessCheckInstaller;
+use Magento\Setup\Controller\ReadinessCheckUpdater;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Environment check Php console command
+ */
+class Php extends Command
+{
+    const TYPE_OPTION = 'type';
+
+    /**
+     * File system
+     *
+     * @var \Magento\Framework\Filesystem
+     */
+    protected $filesystem;
+
+    /**
+     * Cron Script Readiness Check
+     *
+     * @var \Magento\Setup\Model\CronScriptReadinessCheck
+     */
+    protected $cronScriptReadinessCheck;
+
+    /**
+     * PHP Readiness Check
+     *
+     * @var \Magento\Setup\Model\PhpReadinessCheck
+     */
+    protected $phpReadinessCheck;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\Setup\FilePermissions $permissions
+     * @param \Magento\Framework\Filesystem $filesystem
+     * @param \Magento\Setup\Model\CronScriptReadinessCheck $cronScriptReadinessCheck
+     * @param \Magento\Setup\Model\PhpReadinessCheck $phpReadinessCheck
+     */
+    public function __construct(
+        \Magento\Framework\Setup\FilePermissions $permissions,
+        \Magento\Framework\Filesystem $filesystem,
+        \Magento\Setup\Model\CronScriptReadinessCheck $cronScriptReadinessCheck,
+        \Magento\Setup\Model\PhpReadinessCheck $phpReadinessCheck,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\App\State $state,
+        \Magento\Framework\Stdlib\DateTime\DateTime $dateTime
+    ) {
+        $this->permissions = $permissions;
+        $this->filesystem = $filesystem;
+        $this->cronScriptReadinessCheck = $cronScriptReadinessCheck;
+        $this->phpReadinessCheck = $phpReadinessCheck;
+        $this->logger = $logger;
+        $this->state = $state;
+        $this->dateTime = $dateTime;
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ) {
+        $this->input = $input;
+        $this->output = $output;
+        $this->state->setAreaCode(\Magento\Framework\App\Area::AREA_FRONTEND);
+        $type = $this->input->getOption(self::TYPE_OPTION) ?: 'installer';
+
+        $progress = new ProgressBar($this->output, 2);
+        $progress->start();
+
+        $this->output->writeln('');
+
+        $this->output->writeln((string) __('[%1] Start', $this->dateTime->gmtDate()));
+        $version = $this->phpVersionAction($type);
+        if (isset($version['data']['required'])) {
+            $this->output->writeln((string) __(
+                '[%1] <info>PHP Version</info> Required : %2',
+                $this->dateTime->gmtDate(),
+                $version['data']['required']
+            ));
+        }
+
+        if (isset($version['data']['current'])) {
+            $this->output->writeln((string) __(
+                '[%1] <info>PHP Version</info> Current : %2',
+                $this->dateTime->gmtDate(),
+                $version['data']['current']
+            ));
+        }
+
+        $progress->advance();
+
+        $extensions = $this->phpExtensionsAction($type);
+        if (isset($extensions['data']['required'])) {
+            foreach ($extensions['data']['required'] as $required) {
+                $this->output->writeln((string) __(
+                    '[%1] <info>PHP Extension</info> Required : %2',
+                    $this->dateTime->gmtDate(),
+                    $required
+                ));
+            }
+        }
+
+        if (isset($extensions['data']['missing'])) {
+            if (empty($extensions['data']['missing'])) {
+                $this->output->writeln((string) __(
+                    '[%1] <info>PHP Extension</info> Missing : <info>%2</info>',
+                    $this->dateTime->gmtDate(),
+                    'None'
+                ));
+            }
+            foreach ($extensions['data']['missing'] as $missing) {
+                $this->output->writeln((string) __(
+                    '[%1] <error>PHP Extension</error> Missing : <error>%2</error>',
+                    $this->dateTime->gmtDate(),
+                    $missing
+                ));
+            }
+        }
+
+        $progress->advance();
+
+        $progress->finish();
+        $this->output->writeln('');
+        $this->output->writeln((string) __('[%1] Finish', $this->dateTime->gmtDate()));
+    }
+
+    /**
+     * Verifies php version
+     * @return \Zend\View\Model\JsonModel
+     */
+    public function phpVersionAction($type)
+    {
+        $data = [];
+        if ($type == ReadinessCheckInstaller::INSTALLER) {
+            $data = $this->phpReadinessCheck->checkPhpVersion();
+        } elseif ($type == ReadinessCheckUpdater::UPDATER) {
+            $data = $this->getPhpChecksInfo(ReadinessCheck::KEY_PHP_VERSION_VERIFIED);
+        }
+        return $data;
+    }
+
+    /**
+     * Verifies php verifications
+     * @return \Zend\View\Model\JsonModel
+     */
+    public function phpExtensionsAction($type)
+    {
+        $data = [];
+        if ($type == ReadinessCheckInstaller::INSTALLER) {
+            $data = $this->phpReadinessCheck->checkPhpExtensions();
+        } elseif ($type == ReadinessCheckUpdater::UPDATER) {
+            $data = $this->getPhpChecksInfo(ReadinessCheck::KEY_PHP_EXTENSIONS_VERIFIED);
+        }
+        return $data;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName("xigen:check:platform");
+        $this->setDescription("Check platform requirements");
+        $this->setDefinition([
+            new InputOption(self::TYPE_OPTION, '-l', InputOption::VALUE_OPTIONAL, 'Type'),
+
+        ]);
+        parent::configure();
+    }
+}
---
+++ README.md
@@ -0,0 +1,53 @@
+# Php / Environemnt CLI Check
+
+CLI script to check php and environment settings
+
+# Install instructions #
+
+`composer require dominicwatts/environmentcheck`
+
+`php bin/magento setup:upgrade`
+
+# Usage instructions #
+
+Run console command
+
+`xigen:check:platform [-t|--type TYPE]`
+
+    php/bin magento xigen:check:platform
+
+    php/bin magento xigen:check:platform -t installer
+
+    php/bin magento xigen:check:platform -t updater
+
+## Example Output
+
+```
+0/2 [>---------------------------]   0%
+[2020-03-18 15:10:35] Start
+[2020-03-18 15:10:35] PHP Version Required : ~7.1.3||~7.2.0||~7.3.0
+[2020-03-18 15:10:35] PHP Version Current : 7.2.23
+ 1/2 [==============>-------------]  50%[2020-03-18 15:10:35] PHP Extension Required : curl
+[2020-03-18 15:10:35] PHP Extension Required : iconv
+[2020-03-18 15:10:35] PHP Extension Required : mbstring
+[2020-03-18 15:10:35] PHP Extension Required : dom
+[2020-03-18 15:10:35] PHP Extension Required : hash
+[2020-03-18 15:10:35] PHP Extension Required : openssl
+[2020-03-18 15:10:35] PHP Extension Required : xmlwriter
+[2020-03-18 15:10:35] PHP Extension Required : pcre
+[2020-03-18 15:10:35] PHP Extension Required : json
+[2020-03-18 15:10:35] PHP Extension Required : gd
+[2020-03-18 15:10:35] PHP Extension Required : bcmath
+[2020-03-18 15:10:35] PHP Extension Required : simplexml
+[2020-03-18 15:10:35] PHP Extension Required : spl
+[2020-03-18 15:10:35] PHP Extension Required : xsl
+[2020-03-18 15:10:35] PHP Extension Required : intl
+[2020-03-18 15:10:35] PHP Extension Required : ctype
+[2020-03-18 15:10:35] PHP Extension Required : pdo_mysql
+[2020-03-18 15:10:35] PHP Extension Required : soap
+[2020-03-18 15:10:35] PHP Extension Required : zip
+[2020-03-18 15:10:35] PHP Extension Required : libxml
+[2020-03-18 15:10:35] PHP Extension Missing : None
+ 2/2 [============================] 100%
+[2020-03-18 15:10:35] Finish
+```
